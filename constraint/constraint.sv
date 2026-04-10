1216d142adea1a4d78043a9c80007a70aee7ea19
rnaveenprakash100 <rnaveenprakash100@gmail.com>
2026-04-10 14:33:15 +0530

    Update constraint.sv

--- constraint/constraint.sv
+++ constraint/constraint.sv
@@ -18,3 +18,16 @@ module constraint1;
     end
   end
 endmodule
+/* output
+run -all
+# data=588339297,addr=363
+# data=-699949109,addr=498
+# data=277165595,addr=303
+# data=-519089136,addr=448
+# data=-121318706,addr=436
+# data=-937788835,addr=451
+# data=413619939,addr=403
+# data=1132976853,addr=445
+# data=839011722,addr=375
+# data=1840099912,addr=462
+# exit */
